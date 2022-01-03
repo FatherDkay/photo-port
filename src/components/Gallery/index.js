717d0cf98844344b1876fb0be0548943386f5314
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-03 15:17:38 -0800

    gallery index breaks the render

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,25 +1,48 @@
-import React from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
-function Gallery(props) {
-  const currentCategory = {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  };
+//Old Code Before 20.3.5 refactor.  If uncommented and the new code is commented the project
+//renders without any problems
+// import React from "react";
+// import { capitalizeFirstLetter } from '../../utils/helpers';
+// import photo from "../../assets/small/commercial/0.jpg";
+
+// function Gallery(props) {
+//   const currentCategory = {
+//     name: "commercial",
+//     description:
+//       "Photos of grocery stores, food trucks, and other commercial projects",
+//   };
+//   return (
+//     <section>
+//       <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+//       <p>{currentCategory.name}</p>
+//       <div className="flex-row">
+//           <img
+//             src={photo}
+//             alt="Commercial Example"
+//             className="img-thumbnail mx-1"
+//           />
+//       </div>
+//     </section>
+//   );
+// }
+// export default Gallery;
+
+
+//This is the 20.3.5 code copied directly from the lesson.  When this is not commented
+//and the old code (see above) is commented. the project renders blank
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory;
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
-      <div className="flex-row">
-          <img
-            src={photo}
-            alt="Commercial Example"
-            className="img-thumbnail mx-1"
-          />
-      </div>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList  />
     </section>
   );
 }
+
 export default Gallery;
